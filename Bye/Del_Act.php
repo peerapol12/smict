5f0e8533aa5756e92e417485470da03ea5d5ce3b
kungton43 <kungton43@gmail.com>
2015-11-05 16:52:03 +0700

    ลบกิจ

---
+++ Bye/Del_Act.php
@@ -0,0 +1,20 @@
+<?php
+$objConnect = mysql_connect("localhost","root","") or die("Error Connect to Database");
+$objDB = mysql_select_db("test");
+
+$strSQL = "DELETE FROM act_type ";
+$strSQL .="WHERE Act_ID = ".$_GET["Actt"];
+
+$objQuery = mysql_query($strSQL);
+
+if($objQuery)
+{
+	header('location: Setting_Success.php');
+}
+else
+{
+	header('location: Setting_Error.php');
+}
+
+mysql_close($objConnect);
+?>
